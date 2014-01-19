da57debc0a3b8254aaed46efcda6528ea3e3ae2a
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-01-19 10:08:45 -0200

    Elo#expect

--- lib/elo.rb
+++ lib/elo.rb
@@ -6,4 +6,10 @@ class Elo
     @initial = initial
     @beta = beta
   end
+
+  def expect(rate, other_rate)
+    diff = other_rate.to_f - rate.to_f
+    f_factor = 2 * @beta
+    1.to_f / (1 + 10 ** (diff / f_factor))
+  end
 end
--- spec/elo_spec.rb
+++ spec/elo_spec.rb
@@ -8,5 +8,13 @@ describe "Elo" do
       expect(elo.initial).to eq(1200)
       expect(elo.beta).to eq(200)
     end
+
+    it 'should calculate the right expectation' do
+      elo = Elo.new
+      ra = 1200
+      rb = 800
+      expect(elo.expect(ra, rb)).to eq(0.9090909090909091)
+      expect(elo.expect(rb, ra)).to eq(0.09090909090909091)
+    end
   end
 end
