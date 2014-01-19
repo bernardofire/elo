0b0c4ebcc4357a37fb07cec10ae9e071c7e9e812
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-01-19 12:18:17 -0200

    configuration attrs

--- lib/elo.rb
+++ lib/elo.rb
@@ -2,15 +2,20 @@ class Player
   attr_accessor :rating, :games_played, :games
 
   def initialize
-    @rating = 1200
+    @rating = Configuration.new.initial_rating
     @games_played = 0
     @games = []
   end
 end
 
 class Configuration
-  attr_reader :initial_rating
+  attr_reader :initial_rating, :beginner_start, :pro_start, :k_factor
   def initialize
-    @initial_rating = 1200
+    @initial_rating = 1000
+    @beginner_start = 30
+    @pro_start = 2400
+    @k_factor = 15
   end
 end
+
+
--- spec/elo_spec.rb
+++ spec/elo_spec.rb
@@ -4,7 +4,7 @@ describe "Elo" do
   describe Player do
     it 'should create player with default arguments' do
       p = Player.new
-      expect(p.rating).to eq(1200)
+      expect(p.rating).to eq(1000)
       expect(p.games).to eq([])
       expect(p.games_played).to eq(0)
     end
@@ -13,7 +13,10 @@ describe "Elo" do
   describe Configuration do
     it 'should create player with default arguments' do
       c = Configuration.new
-      expect(c.initial_rating).to eq(1200)
+      expect(c.initial_rating).to eq(1000)
+      expect(c.beginner_start).to eq(30)
+      expect(c.pro_start).to eq(2400)
+      expect(c.k_factor).to eq(15)
     end
   end
 end
